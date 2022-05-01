9f89bdffb4f3dd2948505e5d9aa34721811e08dd
Yurii Bilous <yurabilous7@gmail.com>
2022-05-01 13:26:20 +0300

    ui componnent module practice , add default values and add the template html file

--- src/app/code/Elogic/UiMage/view/frontend/web/js/plain-view-model.js
+++ src/app/code/Elogic/UiMage/view/frontend/web/js/plain-view-model.js
@@ -1,10 +1,28 @@
 
 // 1) create first ui component
+// define(['uiElement'], function(UiElement) {
+//     'use strict';
+//
+//     return UiElement.extend({
+//         label: 'My first ui component'
+//     });
+//
+// });
+
+
+
+//2) get template
+
 define(['uiElement'], function(UiElement) {
     'use strict';
 
     return UiElement.extend({
-        label: 'My first ui component'
+        defaults: {
+            template: 'Elogic_UiMage/ui-component',
+            label: 'Some random numbers:',
+            values: [22, 33, 1, 15]
+        }
+
     });
 
 });
