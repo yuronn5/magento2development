1b52bb5bb203ef31fdd63c1ba8928781320c0b07
Yurii Bilous <yurabilous7@gmail.com>
2022-05-01 12:04:31 +0300

    kopractice module add observable to track our title changes

--- src/app/code/Elogic/KoMage/view/frontend/web/js/plain-view-model.js
+++ src/app/code/Elogic/KoMage/view/frontend/web/js/plain-view-model.js
@@ -1,10 +1,23 @@
 // 1) simple
-define([], function() {
+// define([], function() {
+//     'use strict';
+//
+//     return function () {
+//         return {
+//             title: "this is a title for view model by KoMage module"
+//         }
+//     }
+// });
+
+//2) with tracking observable to track changes with our title
+// in console to track ui component use require('uiRegistry').get('startSimple')
+// to change file require('uiRegistry').get('startSimple').title("Surprise") and title will be changed
+define(['ko'], function(ko) {
     'use strict';
 
     return function () {
         return {
-            title: "this is a title for view model by KoMage module"
+            title: ko.observable("this is a title for view model by KoMage module")
         }
     }
 });
